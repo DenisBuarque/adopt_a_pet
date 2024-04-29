360be4ed6ff5d013202dd2b2a6e9aba4630ec90b
denisbuarque <denisprogramadorweb@gmail.com>
2024-04-29 18:26:10 -0300

    Atualizando botão de like

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,22 +1,23 @@
-import api from '../utils/api';
+import api from "../utils/api";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
+import useFlashMessage from "../hooks/useFlashMessage";
 
-const Like = ({ pet, handleLike }) => {
+const Like = ({ pet }) => {
   const [color, setColor] = useState("text-white");
-  const [userId, setUserId] = useState(pet.user._id);
-  const [petId, setPetId] = useState(pet._id);
+  const [petId, setPetId] = useState(pet._id || "");
 
-  const [token] = useState(localStorage.getItem('token') || "");
+  const [token] = useState(localStorage.getItem("token") || "");
 
-  async function like(e) {
+  const { setMessage } = useFlashMessage();
+
+  async function handleLike(e) {
     e.preventDefault();
 
     let msgType = "bg-green-600";
 
     const formData = new FormData();
-    formData.append("userId", userId);
     formData.append("petId", petId);
 
     const data = await api
@@ -33,16 +34,30 @@ const Like = ({ pet, handleLike }) => {
         return error.response.data;
       });
 
+    setMessage(data.message, msgType);
     setColor("text-red-500");
+  }
+
+  function loginError() {
+    let msgType = "bg-red-600";
+    let msgText = "Você deve fazer o login para curtir o Pet.";
+    setMessage(msgText, msgType);
   }
 
   return (
     <>
-      <form onSubmit={like}>
-        <button type="submit">
-          <FaHeart className={`w-7 h-7 mb-3 ${color}`} />
-        </button>
-      </form>
+      {!token ? (
+        <Link onClick={loginError}>
+          <FaHeart className={`w-7 h-7 mb-3 text-white`} />
+        </Link>
+      ) : (
+        <form onSubmit={handleLike}>
+          <input type="hidden" name="petId" value={petId} onChange={(e) => setPetId(e.target.value)} />
+          <button type="submit">
+            <FaHeart className={`w-7 h-7 mb-3 text-white`} />
+          </button>
+        </form>
+      )}
     </>
   );
 };
