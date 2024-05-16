53ed47787f93ed071fb2fd3929732517c263d239
denisbuarque <denisprogramadorweb@gmail.com>
2024-05-16 18:14:48 -0300

    Atualização de arquivo verificação de token

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -3,8 +3,6 @@ const getToken = require('./getToken');
 
 const verifyToken = (req, res, next) => {
 
-    console.log(req.headers);
-
     if(!req.headers.authorization) {
         res.status(401).json({ message: "Você não tem autorização!"});
         return;
